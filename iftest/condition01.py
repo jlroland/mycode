97273eba0db0f869ca8c355e9b38e3cfef17a1f2
Jessica Roland <jlroland@users.noreply.github.com>
2022-04-19 20:34:29 +0000

    Labs 24,25--demonstrate conditionals

---
+++ iftest/condition01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#flowchart TD
+#    A[Check Our String] --> B{"Is it 'MTG'?"};
+#    B -- Yes --> C[print it out!]
+#    B -- No --> D[do nothing]
+#    C --> E[Logic Done]
+#    D --> E[Logic Done]
+
+# create the string hostname
+hostname = "MTG"
+# test logic with the `if` statement
+# what to do if this statement is found to be true
+if hostname == "MTG":
+    print("The hostname was found to be MTG")
+
